bd389e5c0fbf9657c6022832377e207c8884e88c
le-it-24 <le.it.pro@outlook.com>
2023-09-06 14:30:48 -0700

    first commit

---
+++ include/VoltageSensor.h
@@ -0,0 +1,61 @@
+
+
+
+
+/*
+* VoltageSensor.h
+*
+* Leonard K. Ebron
+* LE.it.pro@outlook.com
+* 
+* Wednesday, 6TH September 2023
+*/
+
+
+
+
+class VoltageSensor // Defines a class of voltage divider based input voltage sensing device objects.
+{
+
+
+
+
+    private:
+
+
+
+
+        // Voltage divider components:
+
+
+        double resistor1; // Value of resistor (in ohms) closest to the input voltage supply (The voltage to be calculated).
+        double resistor2; // Value of resistor2 (in ohms).
+
+
+        // Voltage divider attributes:
+
+
+        double Vin; // The input voltage, this needs to be calculated.
+        double Vsense; // This is the voltage measured at the ADC input. This parameter is used, with resistor1-2, to calculate
+                       // Vin.
+
+
+
+
+    public:
+
+
+        // Getters and Setter methods of VoltageSensor.
+
+
+        void set_resistor1_val(double value);
+        void set_resistor2_val(double value);
+
+
+        void get_Vsense();
+        void get_Vin();
+        
+        
+            
+        
+};
---
+++ src/VoltageSensor.cpp
@@ -0,0 +1,25 @@
+
+
+
+
+#include <Arduino.h>
+#include <VoltageSensor.h>
+
+
+// Vout = Vin * R2/R1 + R2
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
